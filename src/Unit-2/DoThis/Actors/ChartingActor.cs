478c274f181dc263df6711d4530df95c0b3748eb
Steven Mitchell <quatzequatel@hotmail.com>
2019-09-10 13:00:41 -0700

    merge from unit2?

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
 using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : UntypedActor
+    public class ChartingActor : ReceiveActor, IWithUnboundedStash
     {
         #region Messages
 
@@ -20,28 +21,65 @@ namespace ChartApp.Actors
             public Dictionary<string, Series> InitialSeries { get; private set; }
         }
 
+        public class AddSeries
+        {
+            public AddSeries(Series series)
+            {
+                Series = series;
+            }
+
+            public Series Series { get; private set; }
+        }
+        
+        /// <summary>
+        /// Remove an existing <see cref="Series"/> from the chart
+        /// </summary>
+        public class RemoveSeries
+        {
+            public RemoveSeries(string seriesName)
+            {
+                SeriesName = seriesName;
+            }
+
+            public string SeriesName { get; private set; }
+        }
+
+        // Actors/ChartingActor.cs - add inside the Messages region
+        /// <summary>
+        /// Toggles the pausing between charts
+        /// </summary>
+        public class TogglePause { }
+
         #endregion
+
+        /// <summary>
+        /// Maximum number of points we will allow in a series
+        /// </summary>
+        public const int MaxPoints = 250;
+
+        /// <summary>
+        /// Incrementing counter we use to plot along the X-axis
+        /// </summary>
+        private int xPosCounter = 0;
 
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
+        private readonly Button _pauseButton;
 
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
+        public IStash Stash { get; set; }
+
+        public ChartingActor(Chart chart, Button pauseButton) :
+            this(chart, new Dictionary<string, Series>(), pauseButton)
         {
         }
 
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex,
+            Button pauseButton)
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
-        }
-
-        protected override void OnReceive(object message)
-        {
-            if (message is InitializeChart)
-            {
-                var ic = message as InitializeChart;
-                HandleInitialize(ic);
-            }
+            _pauseButton = pauseButton;
+            Charting();
         }
 
         #region Individual Message Type Handlers
@@ -57,6 +95,13 @@ namespace ChartApp.Actors
             //delete any existing series
             _chart.Series.Clear();
 
+            // set the axes up
+            var area = _chart.ChartAreas[0];
+            area.AxisX.IntervalType = DateTimeIntervalType.Number;
+            area.AxisY.IntervalType = DateTimeIntervalType.Number;
+
+            SetChartBoundaries();
+
             //attempt to render the initial chart
             if (_seriesIndex.Any())
             {
@@ -67,8 +112,110 @@ namespace ChartApp.Actors
                     _chart.Series.Add(series.Value);
                 }
             }
+
+            SetChartBoundaries();
+        }
+
+        private void HandleAddSeries(AddSeries series)
+        {
+            if(!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
+            {
+                _seriesIndex.Add(series.Series.Name, series.Series);
+                _chart.Series.Add(series.Series);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleRemoveSeries(RemoveSeries series)
+        {
+            if (!string.IsNullOrEmpty(series.SeriesName) &&
+                _seriesIndex.ContainsKey(series.SeriesName))
+            {
+                var seriesToRemove = _seriesIndex[series.SeriesName];
+                _seriesIndex.Remove(series.SeriesName);
+                _chart.Series.Remove(seriesToRemove);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetrics(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) &&
+                _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, metric.CounterValue);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
+        private void HandleMetricsPaused(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series)
+                && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                // set the Y value to zero when we're paused
+                series.Points.AddXY(xPosCounter++, 0.0d);
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
         }
 
         #endregion
+        private void SetChartBoundaries()
+        {
+            double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
+            var allPoints = _seriesIndex.Values.SelectMany(series => series.Points).ToList();
+            var yValues = allPoints.SelectMany(point => point.YValues).ToList();
+            maxAxisX = xPosCounter;
+            minAxisX = xPosCounter - MaxPoints;
+            maxAxisY = yValues.Count > 0 ? Math.Ceiling(yValues.Max()) : 1.0d;
+            minAxisY = yValues.Count > 0 ? Math.Floor(yValues.Min()) : 0.0d;
+            if (allPoints.Count > 2)
+            {
+                var area = _chart.ChartAreas[0];
+                area.AxisX.Minimum = minAxisX;
+                area.AxisX.Maximum = maxAxisX;
+                area.AxisY.Minimum = minAxisY;
+                area.AxisY.Maximum = maxAxisY;
+            }
+        }
+
+        private void Charting()
+        {
+            Receive<InitializeChart>(ic => HandleInitialize(ic));
+            Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
+            Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
+            Receive<Metric>(metric => HandleMetrics(metric));
+
+            //new receive handler for the TogglePause message type
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(true);
+                BecomeStacked(Paused);
+            });
+        }
+
+        private void SetPauseButtonText(bool paused)
+        {
+            _pauseButton.Text = string.Format("{0}", !paused ? "PAUSE ||" : "RESUME ->");
+        }
+
+        private void Paused()
+        {
+            // while paused, we need to stash AddSeries & RemoveSeries messages
+            Receive<AddSeries>(addSeries => Stash.Stash());
+            Receive<RemoveSeries>(removeSeries => Stash.Stash());
+            Receive<Metric>(metric => HandleMetricsPaused(metric));
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(false);
+                UnbecomeStacked();
+                // ChartingActor is leaving the Paused state, put messages back
+                // into mailbox for processing under new behavior
+                Stash.UnstashAll();
+            });
+        }
     }
 }
--- src/Unit-2/DoThis/Main.Designer.cs
+++ src/Unit-2/DoThis/Main.Designer.cs
@@ -32,6 +32,10 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnCpu = new System.Windows.Forms.Button();
+            this.BtnMemory = new System.Windows.Forms.Button();
+            this.BtnDisk = new System.Windows.Forms.Button();
+            this.btnPauseResume = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,33 +47,91 @@
             legend1.Name = "Legend1";
             this.sysChart.Legends.Add(legend1);
             this.sysChart.Location = new System.Drawing.Point(0, 0);
+            this.sysChart.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.sysChart.Name = "sysChart";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.sysChart.Series.Add(series1);
-            this.sysChart.Size = new System.Drawing.Size(684, 446);
+            this.sysChart.Size = new System.Drawing.Size(1824, 1063);
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
+            // BtnCpu
+            // 
+            this.BtnCpu.AutoSize = true;
+            this.BtnCpu.Location = new System.Drawing.Point(1545, 704);
+            this.BtnCpu.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.BtnCpu.Name = "BtnCpu";
+            this.BtnCpu.Size = new System.Drawing.Size(180, 73);
+            this.BtnCpu.TabIndex = 1;
+            this.BtnCpu.Text = "CPU (ON)";
+            this.BtnCpu.UseVisualStyleBackColor = true;
+            this.BtnCpu.Click += new System.EventHandler(this.BtnCpu_Click);
+            // 
+            // BtnMemory
+            // 
+            this.BtnMemory.AutoSize = true;
+            this.BtnMemory.Location = new System.Drawing.Point(1545, 798);
+            this.BtnMemory.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.BtnMemory.Name = "BtnMemory";
+            this.BtnMemory.Size = new System.Drawing.Size(244, 73);
+            this.BtnMemory.TabIndex = 2;
+            this.BtnMemory.Text = "MEMORY (OFF)";
+            this.BtnMemory.UseVisualStyleBackColor = true;
+            this.BtnMemory.Click += new System.EventHandler(this.BtnMemory_Click);
+            // 
+            // BtnDisk
+            // 
+            this.BtnDisk.AutoSize = true;
+            this.BtnDisk.Location = new System.Drawing.Point(1545, 894);
+            this.BtnDisk.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.BtnDisk.Name = "BtnDisk";
+            this.BtnDisk.Size = new System.Drawing.Size(244, 73);
+            this.BtnDisk.TabIndex = 3;
+            this.BtnDisk.Text = "DISK (OFF)";
+            this.BtnDisk.UseVisualStyleBackColor = true;
+            this.BtnDisk.Click += new System.EventHandler(this.BtnDisk_Click);
+            // 
+            // btnPauseResume
+            // 
+            this.btnPauseResume.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPauseResume.Location = new System.Drawing.Point(1545, 496);
+            this.btnPauseResume.Name = "btnPauseResume";
+            this.btnPauseResume.Size = new System.Drawing.Size(244, 73);
+            this.btnPauseResume.TabIndex = 4;
+            this.btnPauseResume.Text = "PAUSE ||";
+            this.btnPauseResume.UseVisualStyleBackColor = true;
+            this.btnPauseResume.Click += new System.EventHandler(this.BtnPauseResume_Click);
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 446);
+            this.ClientSize = new System.Drawing.Size(1824, 1063);
+            this.Controls.Add(this.btnPauseResume);
+            this.Controls.Add(this.BtnDisk);
+            this.Controls.Add(this.BtnMemory);
+            this.Controls.Add(this.BtnCpu);
             this.Controls.Add(this.sysChart);
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.Name = "Main";
             this.Text = "System Metrics";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
             this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
+        private System.Windows.Forms.Button BtnCpu;
+        private System.Windows.Forms.Button BtnMemory;
+        private System.Windows.Forms.Button BtnDisk;
+        private System.Windows.Forms.Button btnPauseResume;
     }
 }
 
